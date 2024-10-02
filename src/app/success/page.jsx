e3e1d85a0805b857f64b3cb70a20796bd5bf1624
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-10-01 21:37:26 -0800

    Update page.jsx

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,25 +1,7 @@
 "use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 import Confetti from "react-confetti";
 
 const SuccessPage = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const orderId = searchParams.get("orderId");
-
-  useEffect(() => {
-    if (!orderId) return;
-
-    const timer = setTimeout(() => {
-      router.push("/orders/" + orderId);
-    }, 5000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [orderId, router]);
 
   return (
     <div className="container overflow-hidden">
